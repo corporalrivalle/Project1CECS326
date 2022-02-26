4b1f6fe4b16ef051b210efd9165e5db2945df41c
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2022-02-26 12:57:04 -0800

    Added comments to Q1

--- Q1/QuoteClient.java
+++ Q1/QuoteClient.java
@@ -10,16 +10,17 @@ public class QuoteClient
 {
 	public static void main(String[] args) {
 		try{
-			Socket sock = new Socket("127.0.0.1",6018);
-			InputStream input = sock.getInputStream();
-			BufferedReader bin = new BufferedReader(new InputStreamReader(input));
+			Socket sock = new Socket("127.0.0.1",6018); /*Creates a socket on port 6108, ip 127.0.0.1*/
+			InputStream input = sock.getInputStream(); /*creates an input string*/
+			BufferedReader bin = new BufferedReader(new InputStreamReader(input)); /*creates an output string*/
 			String line;
 			while ((line=bin.readLine()) !=null) {
-				System.out.println(line);
+				System.out.println(line); /*prints out any output that the server gives it*/
 			}
-			sock.close();
-		} catch (IOException ioe) {
+			sock.close(); /*closes the socket*/
+		} catch (IOException ioe) { /*Exception handling for In/Out Error*/
 			System.err.println(ioe);
 		}
 	}
 }
+
--- Q1/QuoteServer.java
+++ Q1/QuoteServer.java
@@ -9,17 +9,17 @@ import java.io.*;
 public class QuoteServer
 {
   public static void main(String[] args) {
-    String qotd = "Carpe Diem!";
+    String qotd = "Carpe Diem!"; /*Creates a quote of the day (qtod)*/
     try{
-      ServerSocket sock = new ServerSocket(6018);
-      while(true){
-        Socket client = sock.accept();
-        PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-        pout.println(qotd); /*The problem where it prints out the date*/
-        client.close();
+      ServerSocket sock = new ServerSocket(6018); /*Server turns on to listen to port 6018*/
+      while(true){ /*will keep listening until a client connects*/
+        Socket client = sock.accept(); /*accepts any request to connect on port 6018*/
+        PrintWriter pout = new PrintWriter(client.getOutputStream(), true); /*Creates an object that gives the client a command*/
+        pout.println(qotd); /*gives the qotd to the client*/
+        client.close(); /*closes connection with client*/
       }
     }
-    catch (IOException ioe) {
+    catch (IOException ioe) { /*Exception handling for In/Out Errors*/
       System.err.println(ioe);
     }
   }
