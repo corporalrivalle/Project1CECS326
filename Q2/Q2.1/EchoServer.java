d6a1c6f29c42ba5e43a38b4d0c1a1b0e24cc9d60
BranDenn <76827979+BranDenn@users.noreply.github.com>
2022-02-27 20:23:05 -0800

    Add files via upload

--- Q2/Q2.1/EchoServer.java
+++ Q2/Q2.1/EchoServer.java
@@ -1,6 +1,6 @@
-/**
- * An echo server listening on port 6007. This server reads from the client
- * and echoes back the result. 
+/*
+ * An echo server listening on port 6017. 
+ * This server reads information sent by the client and echoes it back. 
  */
 
 import java.net.*;
@@ -8,50 +8,51 @@ import java.io.*;
 
 public class  EchoServer
 {
-    public static void main(String[] args) 
-    {
-            System.out.println("Server Started!");
-            try
-            {
-                // Declare server socket on port 6017
-                ServerSocket sock = new ServerSocket(6018);
-    
-                // Wait for the clien to connect to the server
-                System.out.println("Waiting for client...");
-                Socket client = sock.accept();
-    
-                // Display confirmation of client connection
-                System.out.println("Client connected!");
-    
-                // Wait for client input to echo
-                System.out.println("Waiting for client input...");
-    
-                // get the client input (keyboard input from client saved into input stream system.in)
-                BufferedReader bin = new BufferedReader(new InputStreamReader(client.getInputStream()));
-    
-                // save information as a string (serverEcho) so any information can be displayed (numbers conflict with string concatenation)
-                String serverEcho = bin.readLine();
-                String clientEcho ="";
+	public static void main(String[] args) 
+	{
+		try
+		{
+			System.out.println("Server Started!");
+			
+			// Declare server socket on port 6017
+			ServerSocket sock = new ServerSocket(6017);
 
-                //Changes output to correctly identify server/client. If no server/client info is given, an error message is returned instead.
-                if (serverEcho.contains("client")){
-                    clientEcho = "Hi! I am the server!";
-                } else {
-                    clientEcho = serverEcho;
-                }
-    
-                // Display what the server has received
-                System.out.println("Client has entered '" + serverEcho + "'");
-    
-                // Echo back what the server received (echo back client input to client)
-                System.out.println("Echoing information recieved back to client...");
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-                pout.println("Server Echo: " + clientEcho);
+			// while loop to keep server running
+			while (true)
+			{
+				// Wait for the clien to connect to the server
+				System.out.println("Waiting for client...");
+				Socket client = sock.accept();
+	
+				// Display confirmation of client connection
+				System.out.println("Client connected!");
+	
+				// Wait for client input to echo
+				System.out.println("Waiting for client input...");
+	
+				// get the client input (keyboard input from client saved into input stream system.in)
+				BufferedReader bin = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				
+				// save information as a string (serverEcho) so any information can be displayed (numbers conflict with string concatenation)
+				String serverEcho = bin.readLine();
+	
+				// Display what the server has received
+				System.out.println("Client has entered '" + serverEcho + "'");
+	
+				// Echo back what the server received (echo back client input to client)
+				System.out.println("Echoing information recieved back to client...");
+				PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+				pout.println("Server Echo: " + serverEcho);
 
-                System.out.println("Terminating Connection...");
-            }
-            catch (IOException ioe) {
-                System.err.println(ioe);
-            }
-        }
-    }
+				// close client connection
+				System.out.println("Closing client connection and allowing for new one... \n");
+				client.close();
+			}
+		}
+
+		catch (IOException ioe) 
+		{
+			System.err.println(ioe);
+		}
+	}
+}
